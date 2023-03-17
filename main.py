8febf172c1a26e317332899647ae8f4d718bdce3
Seb <65369608+sebr22@users.noreply.github.com>
2023-03-17 00:13:49 -0400

    gps test

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ import adafruit_lps2x
 #sudo pip3 install adafruit-circuitpython-bh1750
 import adafruit_bh1750
 
+#sudo pip3 install adafruit-circuitpython-gps
+import adafruit_gps
+
 import csv
 
 #CO2
@@ -34,6 +37,13 @@ fp.close()
 i2cPress = busio.I2C(board.SCL, board.SDA)
 lps = adafruit_lps2x.LPS25(i2cPress)
 
+#gps
+uart = busio.UART(board.TX, board.RX, baudrate=9600, timeout=10)
+gps = adafruit_gps.GPS(uart, debug=False)
+gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+gps.send_command(b"PMTK220,1000")
+
+
 scd4x.start_periodic_measurement()
 print("Waiting for first measurement....")
 
@@ -43,6 +53,57 @@ with open(file, mode='a') as list:
 
 while True:
     if scd4x.data_ready:
+        #gps
+        gps.update()
+        # Every second print out current location details if there's a fix.
+        current = time.monotonic()
+        if current - last_print >= 1.0:
+            last_print = current
+            if not gps.has_fix:
+                # Try again if we don't have a fix yet.
+                print("Waiting for fix...")
+                continue
+            # We have a fix! (gps.has_fix is true)
+            # Print out details about the fix like location, date, etc.
+            print("=" * 40)  # Print a separator line.
+            print(
+                "Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}".format(
+                    gps.timestamp_utc.tm_mon,  # Grab parts of the time from the
+                    gps.timestamp_utc.tm_mday,  # struct_time object that holds
+                    gps.timestamp_utc.tm_year,  # the fix time.  Note you might
+                    gps.timestamp_utc.tm_hour,  # not get all data like year, day,
+                    gps.timestamp_utc.tm_min,  # month!
+                    gps.timestamp_utc.tm_sec,
+                )
+            )
+            print("Latitude: {0:.6f} degrees".format(gps.latitude))
+            print("Longitude: {0:.6f} degrees".format(gps.longitude))
+            print(
+                "Precise Latitude: {:2.}{:2.4f} degrees".format(
+                    gps.latitude_degrees, gps.latitude_minutes
+                )
+            )
+            print(
+                "Precise Longitude: {:2.}{:2.4f} degrees".format(
+                    gps.longitude_degrees, gps.longitude_minutes
+                )
+            )
+            print("Fix quality: {}".format(gps.fix_quality))
+            # Some attributes beyond latitude, longitude and timestamp are optional
+            # and might not be present.  Check if they're None before trying to use!
+            if gps.satellites is not None:
+                print("# satellites: {}".format(gps.satellites))
+            if gps.altitude_m is not None:
+                print("Altitude: {} meters".format(gps.altitude_m))
+            if gps.speed_knots is not None:
+                print("Speed: {} knots".format(gps.speed_knots))
+            if gps.track_angle_deg is not None:
+                print("Track angle: {} degrees".format(gps.track_angle_deg))
+            if gps.horizontal_dilution is not None:
+                print("Horizontal dilution: {}".format(gps.horizontal_dilution))
+            if gps.height_geoid is not None:
+                print("Height geoid: {} meters".format(gps.height_geoid))
+        #end gps
         print("CO2: %d ppm" % scd4x.CO2)
         print("Humidity: %0.1f %%" % scd4x.relative_humidity)
         print("Pressure: %.2f hPa" % lps.pressure)
